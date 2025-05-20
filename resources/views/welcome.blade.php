613436d269294e49afa33f6558d029e8e144fcd5
zayaan <ahmedzayaanshafiu@gmail.com>
2025-05-20 16:21:12 +0500

    tailwind initialised :)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,15 @@
-<?php 
-    echo"blogging site for bloggers to post blogs!";
-?>
+<!DOCTYPE html>
+<html> 
+
+<head>
+    @vite(['resources/css/app.css'])
+</head>
+<body>
+ <div class="bg-red-400 text-black text-center">
+  If this box is red with white text, Tailwind is working!
+</div>
+
+
+</body>
+
+</html>
