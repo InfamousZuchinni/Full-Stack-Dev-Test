3849b8126ec9d62a48112d2c738c06c2a96f72fb
zayaan <ahmedzayaanshafiu@gmail.com>
2025-05-25 12:21:21 +0500

    removed useless DB and migrations and added post saving feat

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,8 +11,5 @@ class Post extends Model
 
 protected $fillable = ['title', 'slug', 'image', 'excerpt', 'content'];
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+    
 }
---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class PostSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('posts')->insert([
+            [
+                'title' => 'First Post',
+                'slug' => Str::slug('First Post'),
+                'excerpt' => 'This is the excerpt of the first post.',
+                'content' => 'This is the full content of the first post.',
+                'image' => 'sample-images/sample1.jpg', // image path relative to public/
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'Second Post',
+                'slug' => Str::slug('Second Post'),
+                'excerpt' => 'Excerpt for the second post.',
+                'content' => 'Full content for the second post.',
+                'image' => 'sample-images/sample2.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+
+         $this->call([
+            PostSeeder::class,
+        ]);
+    }
+
+  
+
+}
